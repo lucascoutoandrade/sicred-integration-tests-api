b730c640052f959db2594ce15e2322cc33f4efca
rodrigolimaqa <55006541+RodrigoQA@users.noreply.github.com>
2023-03-13 16:39:48 -0300

    Update Constantes.java

--- src/main/java/br/com/sicred/core/Constantes.java
+++ src/main/java/br/com/sicred/core/Constantes.java
@@ -5,7 +5,7 @@ import io.restassured.http.ContentType;
 
 public interface Constantes {
     String APP_BASE_URL = "http://127.0.0.1/api/v1/";
-    Integer APP_PORT = 8001;
+    Integer APP_PORT = 8080;
     String APP_BASE_PATH = "api/v1/";
 
     ContentType APP_CONTENT_TYPE = ContentType.JSON;
